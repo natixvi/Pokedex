169f577879c3e1716f322059a3564deb18db5646
Marek Polek <marek.pawel.polek@gmail.com>
2024-07-08 20:22:42 +0200

    before change version

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- pokedex/src/app/components/navbar/navbar.component.ts
+++ pokedex/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [PanelMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+ 
+  items?: MenuItem[];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.items =[
+        {
+          label: 'Files',
+          icon: 'pi pi-file',
+          items: [
+            {label: 'Gen-1',},
+            {label: 'Gen-2',},
+            {label: 'Gen-3',}
+            
+          ]
+        }
+    ]
+  }
 
 }
