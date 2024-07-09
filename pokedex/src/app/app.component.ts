8810e3d5d2a38ceb89aa1fafabf271d61a1ad74b
Natalia <natixkol@gmail.com>
2024-07-09 12:37:25 +0200

    remove unused linies

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { PokemonService } from './services/pokemon.service';
-import { PokemonDetails } from './models/pokemonDetails';
-import { PokemonResponse } from './models/pokemonResponse';
 
 @Component({
   selector: 'app-root',
@@ -15,26 +12,4 @@ import { PokemonResponse } from './models/pokemonResponse';
 })
 export class AppComponent {
   title = 'pokedex';
-
-  pokemon: PokemonDetails = {} as PokemonDetails;
-  pokemonResponse: PokemonResponse = {} as PokemonResponse;
-
-  constructor(private pokemonService: PokemonService){}
-
-  onClickDetails(){
-    this.pokemonService.getPokemonByName('bulbasaur').subscribe( (response) => {
-      this.pokemon =  response;
-    })
-  }
-
-  onClickAll(){
-      this.pokemonService.getPokemons(151).subscribe( (response) => {
-        this.pokemonResponse = response;
-      })
-  }
-
-  view(){
-    console.log('Pokemon Details:', this.pokemon);
-    console.log('All Pokemons:', this.pokemonResponse);
-  }
 }
