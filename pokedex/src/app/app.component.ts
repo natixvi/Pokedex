96ebef0c6364cbdfee9f939f4e51bdf4e9e8b004
Natalia <natixkol@gmail.com>
2024-07-10 18:37:28 +0200

    add footer

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule,NavbarComponent],
+  imports: [RouterOutlet, ButtonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
