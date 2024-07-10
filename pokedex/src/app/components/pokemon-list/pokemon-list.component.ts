64704ccf6deb053e8f1246871d504f5e8818b16c
Marek Polek <marek.pawel.polek@gmail.com>
2024-07-10 18:19:20 +0200

    create logic to display pokemon

--- pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
+++ pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonResponse } from '../../models/pokemonResponse';
+import { CardModule } from 'primeng/card';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [CardModule,AsyncPipe],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.css'
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
 
+  pokemons: Observable<PokemonResponse> = {} as Observable<PokemonResponse>
+  constructor(private pokemonService: PokemonService){
+
+  }
+
+  ngOnInit(): void {
+   this.pokemons = this.getPokemons(151)
+   
+  }
+
+  getPokemons(limit?:number,offset?: number): Observable<PokemonResponse>{
+    return this.pokemonService.getPokemons(limit,offset)
+  }
+
+
+
+  
 }
